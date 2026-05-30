6cd2dd9f7b26f0d106d39e2452d0e221f1b3d96c
Jason <geneticmedia1@gmail.com>
2026-05-30 23:57:53 +0100

    Track paperBroker casing fix

---
+++ brokers/paperBroker.js
@@ -0,0 +1,44 @@
+const BrokerInterface = require("./brokerInterface");
+
+class PaperBroker extends BrokerInterface {
+  constructor() {
+    super();
+
+    this.balance = 10000;
+
+    this.positions = [];
+  }
+
+  connect() {
+    console.log("Paper broker connected");
+  }
+
+  getBalance() {
+    return this.balance;
+  }
+
+  placeOrder(order) {
+    console.log("Paper order executed:", order);
+
+    this.positions.push(order);
+
+    return {
+      success: true,
+      order,
+    };
+  }
+
+  closePosition(symbol) {
+    this.positions = this.positions.filter(
+      (position) => position.symbol !== symbol
+    );
+
+    console.log(`Closed paper position for ${symbol}`);
+  }
+
+  getOpenPositions() {
+    return this.positions;
+  }
+}
+
+module.exports = PaperBroker;
